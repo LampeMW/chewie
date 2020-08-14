99c87e5488ddb8c26278a62bdd9925d135362796
Matt Lampe <matt@lampe.io>
2020-08-13 21:52:04 -0400

    Fix issue with RouteSettings usage without named routes

--- lib/src/chewie_player.dart
+++ lib/src/chewie_player.dart
@@ -124,7 +124,6 @@ class ChewieState extends State<Chewie> {
   Future<dynamic> _pushFullScreenWidget(BuildContext context) async {
     final isAndroid = Theme.of(context).platform == TargetPlatform.android;
     final TransitionRoute<Null> route = PageRouteBuilder<Null>(
-      settings: RouteSettings(isInitialRoute: false),
       pageBuilder: _fullScreenRoutePageBuilder,
     );
 
